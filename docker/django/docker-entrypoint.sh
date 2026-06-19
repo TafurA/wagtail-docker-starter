67ee595d5db9c4a2df3e00b59d18b8f1422d5aff
TafurA <tafursla@outlook.com>
2026-06-18 23:57:47 -0500

    Feature (Starter): Initial config
    Add initial config for docker, vite and ngnix

---
+++ docker/django/docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Initialize with empty value bye default
+DATABASE_URL=${DATABASE_URL:-""}
+DATABASE_HOST=${DATABASE_HOST:-""}
+chmod +x manage.py
+# Wait for Postgres for Django related commands.
+echo $DATABASE_HOST
+if [[ $@ == *"manage.py "* && ! -z "$DATABASE_HOST" && -z "$DATABASE_URL" ]]
+then
+    RETRIES=60
+    >&2 echo "Waiting for Postgres"
+    until PGPASSWORD=$DATABASE_PASSWORD pg_isready -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER ; do
+        if [ $RETRIES -eq 0 ]; then
+            >&2 echo "Exiting"
+            exit
+        fi
+        >&2 echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts"
+        sleep 1
+    done
+fi
+
+# Run only if django server is being run.
+if [[ $@ == *"manage.py runserver"* ]]
+then
+    # Translations
+    >&2 echo "Compiling Django translations (background job)"
+    ./manage.py compilemessages &
+
+    # Migrations
+    >&2 echo "Running Django migrations (background job)"
+    ./manage.py migrate --noinput &
+    ./manage.py createcachetable &
+
+    # PIP requirements for easy Heroku deploy
+    >&2 echo "Exporting requirements (background job)"
+    poetry export -f requirements.txt -o requirements.txt &
+fi
+
+exec "$@"
